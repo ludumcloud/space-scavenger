6f6bc38f1122206f0785ee8ad1697a9b10b33e62
Parker Diard <prdiard@gmail.com>
2019-10-05 17:36:02 -0400

    added fuel gauge and connected it to ship fuel

--- GUI.gd
+++ GUI.gd
@@ -3,14 +3,21 @@ extends MarginContainer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var metalCapacityBar
+var metalBar
+var fuelBar
+var ship
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	metalCapacityBar = get_child(0).get_child(0).get_child(0);
+	metalBar = get_child(0).get_child(0).get_child(0).get_child(0);
+	fuelBar =  get_child(0).get_child(1).get_child(0);
+	ship = get_parent().get_parent().get_child(0);
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _process(delta):
-	get_child(0).get_child(0).get_child(1).value = get_parent().get_parent().get_child(0).fuel
+	fuelBar.value = ship.fuel;
