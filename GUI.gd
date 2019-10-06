dd2fe66ec335c85669123d460911c4c6ff4a0f05
Mark Abernathy <mark.abernathy@inin.com>
2019-10-06 17:44:40 -0400

    add warp button

--- GUI.gd
+++ GUI.gd
@@ -13,3 +13,4 @@ func _ready():
 func _process(delta):
 	$VBoxContainer/HBoxContainer/FuelCapacity/Fuel.value = ship.fuel
 	$VBoxContainer/HBoxContainer/FuelCapacity.value = ship.fuelMax
+	$VBoxContainer/Button.disabled = (ship.fuel < 200)
