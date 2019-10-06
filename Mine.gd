eecd1e049ba927adcd03442653b9b3de55373bb8
Mark Abernathy <mark.abernathy@inin.com>
2019-10-06 11:53:15 -0400

    Added mine

---
+++ Mine.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func init(position: Vector2):
+	self.global_position = position
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var ship = get_parent().get_child(0)
+	if (ship.global_position - self.global_position).length() < 100:
+		ship.reinit()
+		get_parent().remove_child(self)
