05ff5d67baaa0e0ca6e43d2eb8ee3ba3e855bdd7
Mark Abernathy <mark.abernathy@inin.com>
2019-10-06 18:36:58 -0400

    connect button

--- GUI.gd
+++ GUI.gd
@@ -9,6 +9,10 @@ var ship
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ship = get_parent().get_parent().get_child(0);
+	$VBoxContainer/Button.connect("button_down", self, "handle_warp_button")
+
+func handle_warp_button():
+	print("YOU WARPED")
 
 func _process(delta):
 	$VBoxContainer/HBoxContainer/FuelCapacity/Fuel.value = ship.fuel
