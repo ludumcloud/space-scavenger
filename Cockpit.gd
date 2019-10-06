58b6afb81840523beddf2db0400c49805a83b269
Walter Reel <walter.reel@genesys.com>
2019-10-06 14:23:42 -0400

    More component tweaks

--- Cockpit.gd
+++ Cockpit.gd
@@ -2,7 +2,7 @@ extends Component
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	joints.append(Component.make_joint('hull', get_node('./HullJoint')))
+	joints.append(Component.make_joint(['hull'], get_node('./HullJoint')))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- HullL.gd
+++ HullL.gd
@@ -2,8 +2,4 @@ extends Component
 
 func _ready():
 	joints.append(Component.make_joint(['wing-left'], get_node('./WingLJoint')))
-	joints.append(Component.make_joint(['wing-right'], get_node('./WingRJoint')))
 	joints.append(Component.make_joint(['engine-left', 'hull-left'], get_node('./EngineLJoint')))
-	joints.append(Component.make_joint(['engine-right', 'hull-right'], get_node('./EngineRJoint')))
-
-
