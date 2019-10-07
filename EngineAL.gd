515cf861d65cfe20c85b3f8b19e366c27c062355
Mark Abernathy <mark.abernathy@inin.com>
2019-10-07 11:45:02 -0400

    Toggle particle effects

--- EngineAL.gd
+++ EngineAL.gd
@@ -9,5 +9,8 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		$CPUParticles2D.emitting = true
+	else:
+		$CPUParticles2D.emitting = false
