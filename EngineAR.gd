32803a2cdb5f51d446585d8fc6590dd73c69b206
Mabry Cervin <mabry.cervin@gmail.com>
2019-10-07 12:14:12 -0400

    Fixed particles

--- EngineAR.gd
+++ EngineAR.gd
@@ -10,7 +10,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up") && get_tree().get_root().get_child(0).get_child(0).fuel > 0.1:
 		$CPUParticles2D.emitting = true
 	else:
 		$CPUParticles2D.emitting = false
