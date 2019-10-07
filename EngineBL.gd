a9344da1095a11cb5287e162a009fa6851df0ebe
Mark Abernathy <mark.abernathy@inin.com>
2019-10-07 16:48:54 -0400

    engine trail to alt engines

--- EngineBL.gd
+++ EngineBL.gd
@@ -3,3 +3,9 @@ extends Component
 func _ready():
 	joints.append(Component.make_joint(['wing-left'], get_node('./WingLJoint')))
 	joints.append(Component.make_joint(['engine-left', 'hull-left'], get_node('./EngineLJoint')))
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up") && get_tree().get_root().get_child(0).get_child(0).fuel > 0.1:
+		$CPUParticles2D.emitting = true
+	else:
+		$CPUParticles2D.emitting = false
--- EngineBR.gd
+++ EngineBR.gd
@@ -3,3 +3,9 @@ extends Component
 func _ready():
 	joints.append(Component.make_joint(['wing-right'], get_node('./WingRJoint')))
 	joints.append(Component.make_joint(['engine-right', 'hull-right'], get_node('./EngineRJoint')))
+
+func _process(delta):
+	if Input.is_action_pressed("ui_up") && get_tree().get_root().get_child(0).get_child(0).fuel > 0.1:
+		$CPUParticles2D.emitting = true
+	else:
+		$CPUParticles2D.emitting = false
